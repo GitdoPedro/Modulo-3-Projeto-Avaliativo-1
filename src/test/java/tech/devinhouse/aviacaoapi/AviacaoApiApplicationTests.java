7a2ec47f2f6eddf3118387f2132f2dce086d7db1
GitdoPedro <emailpedrolima@gmail.com>
2023-05-31 11:35:09 -0300

    first commit

---
+++ src/test/java/tech/devinhouse/aviacaoapi/AviacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.aviacaoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
